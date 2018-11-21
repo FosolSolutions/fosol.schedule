35952af827b204bb462093b6a88793cf0f49e9d2
fosol <jeremymfoster@hotmail.com>
2018-11-20 16:07:31 -0800

    Adding api endpoint to create example model.

--- Fosol.Core/Extensions/Types/TypeExtensions.cs
+++ Fosol.Core/Extensions/Types/TypeExtensions.cs
@@ -11,16 +11,41 @@ namespace Fosol.Core.Extensions.Types
 	public static class TypeExtensions
 	{
 		#region Methods
+		/// <summary>
+		/// Determine if the type of the specificed type is a `Nullable<>` type or is a nullable class.
+		/// </summary>
+		/// <param name="type"></param>
+		/// <returns></returns>
 		public static bool IsNullable(this Type type)
 		{
-			return type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>);
+			return type.IsClass || type.IsNullableType();
 		}
 
+		/// <summary>
+		/// Determine if the type of the specificed object is a `Nullable<>` type or is a nullable class.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="obj"></param>
+		/// <returns></returns>
 		public static bool IsNullable<T>(T obj)
 		{
 			return typeof(T).IsNullable();
 		}
+		/// <summary>
+		/// Determine if the type of the specificed type is a `Nullable<>` type..
+		/// </summary>
+		/// <param name="type"></param>
+		/// <returns></returns>
+		public static bool IsNullableType(this Type type)
+		{
+			return type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>);
+		}
 
+		/// <summary>
+		/// Determine if the specified type is enumerable.
+		/// </summary>
+		/// <param name="type"></param>
+		/// <returns></returns>
 		public static bool IsEnumerable(this Type type)
 		{
 			if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(IEnumerable<>)) return true;
